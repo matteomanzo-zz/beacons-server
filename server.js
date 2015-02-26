fefc10d20d08841920bc6355797ebfc670b670ab
Jack Rubio <jackrubio26@gmail.com>
2015-02-26 12:23:44 +0000

    deplying to heroku

--- server.js
+++ server.js
@@ -3,18 +3,41 @@ var app = express();
 var server = require('http').createServer(app);
 
 app.set('view engine', 'ejs');
+app.set("jsonp callback", true);
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/', function(req, res) {
   res.render('index');
+  console.log("homepage")
 });
 
-app.get('/beacon', function(req, res) {
-  console.log('Hello World!');
-  res.render('index');
+app.get('/in', function(req, res) {
+ console.log("in"+req);
+ // res.header("Access-Control-Allow-Origin", "*");
+ // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+ // res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
+ res.jsonp({ "my": "object" });
+});
+
+app.get('/out', function(req, res) {
+  console.log("out")
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
+  res.jsonp({ "my": "object" });
 });
 
 server.listen(process.env.PORT || 9999, function() {
-  console.log('server is listening somewhere');
+  console.log('server is watching you...');
 });
 
-module.exports = server;
+module.exports = server;       
